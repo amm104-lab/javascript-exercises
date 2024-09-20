4dc77c56dab379d407d39416d2ac7ab790b55b56
amm <amm@tutao.de>
2024-09-20 08:33:54 +0200

    Completed task as instructed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,19 @@
-const repeatString = function() {
+
+
+const repeatString = function(string, num) {
+
+    greeting = "";
+
+    if(num >= 0){
+
+    for (let i = 1; i <= num; i++){
+        greeting  =  greeting + string;               
+    };
+    return greeting
+    }
+    else{
+        return "ERROR"
+    }
 
 };
 
