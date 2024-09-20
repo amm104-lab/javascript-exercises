60506bd584b4e55b1b645e4d5d706584125dc0f5
amm <amm@tutao.de>
2024-09-20 10:46:57 +0200

    FInished task according to instructions

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...toRemove) {
+
+    const filteredArray = array.filter(num => num && toRemove.includes(num)===false);
+    return filteredArray
 };
 
 // Do not edit below this line
