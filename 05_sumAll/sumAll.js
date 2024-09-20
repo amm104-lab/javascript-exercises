348702ced4c41746a17f2349fce0bcfee429c021
amm <amm@tutao.de>
2024-09-20 11:27:45 +0200

    Finished task according to instructions

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,22 @@
-const sumAll = function() {
+const sumAll = function(firstNum, lastNum) {
+
+    let sum = 0;
+if(firstNum<0 || Number.isInteger(firstNum)===false || lastNum<0 || Number.isInteger(lastNum)===false){
+    return "ERROR"
+}
+
+ else if(lastNum<firstNum){
+    for(let i = lastNum; i <= firstNum; i++){
+        sum += i
+    }
+}
+else{
+    for(let i = firstNum; i <= lastNum; i++){
+        sum += i
+    }
+}
+
+    return sum
 
 };
 
