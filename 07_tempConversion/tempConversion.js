1f48b47a3ef351f65b42f786adfa7080417ffc83
amm <amm@tutao.de>
2024-09-20 12:13:02 +0200

    Finished task based on instructions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,18 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrentheit) {
+
+  return Math.round(((fahrentheit-32)*(5/9))*10)/10
+
 };
 
-const convertToFahrenheit = function() {
+//x-32*5/9
+
+const convertToFahrenheit = function(celcius) {
+
+  return Math.round((celcius*(9/5)+32)*10)/10
+
 };
+
+//X*9/5+32
 
 // Do not edit below this line
 module.exports = {
