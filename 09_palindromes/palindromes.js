f469083589c221c4860026ce437939c587d9c16a
amm <amm@tutao.de>
2024-10-04 09:47:30 +0200

    Finished excercise according to instructions

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,35 @@
-const palindromes = function () {
+const palindromes = function (string) {
 
-};
+    let firstHalfe = string.toLowerCase().replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, '').replace(/\s+/g, '');  
+    let secondHalfe =  string.toLowerCase().replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, '').replace(/\s+/g, '');
+
+    let firstHalf = firstHalfe.slice((firstHalfe.length/2));
+    let secondHalf = secondHalfe.slice(0,(secondHalfe.length/2)).split("").reverse().join("");
+
+    if(firstHalf.length === secondHalf.length){
+
+        if(firstHalf===secondHalf){
+            return true;
+        }
+
+        else{
+            return false;}
+    }
+
+    else{
+
+    let firstHalfo = firstHalfe.slice((firstHalfe.length/2));
+        let secondHalfo = secondHalfe.slice(0,(secondHalfe.length/2)+1).split("").reverse().join("");
+        
+        if(firstHalfo===secondHalfo){
+            return true;
+            }
+
+        else{
+            return false;}
+
+        }
+    };
 
 // Do not edit below this line
 module.exports = palindromes;
