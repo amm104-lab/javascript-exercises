38e867d2f4940726f932a265b640cebff410b9ce
amm <amm@tutao.de>
2024-10-04 13:11:18 +0200

    Finished excercise according to instructions

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,46 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
 
+    let j = 1;
+    let k = 0;
+    let numb = 0;
+
+    if(typeof(num)=== "string"){
+        let numb = Number(num);
+
+        if(numb === 0){
+            return 0;
+        }
+    
+        else if(numb < 0){
+            return "OOPS";
+        }
+    
+        for(let i=2;i<=numb;i++){
+            let l = j+k;
+            k = j;
+            j = l;
+        }
+        return j;
+    }
+
+    else{
+        let numb = num;
+    
+
+    if(numb === 0){
+        return 0;
+    }
+
+    else if(numb < 0){
+        return "OOPS";
+    }
+
+    for(let i=2;i<=numb;i++){
+        let l = j+k;
+        k = j;
+        j = l;
+    }
+    return j;}
 };
 
 // Do not edit below this line
