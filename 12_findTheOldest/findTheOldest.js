11f595fcae8c7e8a4081afe3e16a6b24eee082e0
amm <amm@tutao.de>
2024-10-07 09:55:43 +0200

    Finished tasks according to instructions

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,18 @@
-const findTheOldest = function() {
+const current =  (new Date()).getFullYear();
+
+const findTheOldest = function(people) {
+
+    /*people.sort((first, second) => (first.people.yearOfDeath - first.people.yearOfBirth)> (second.people.yearOfDeath - second.people.yearOfBirth) ? 1:-1);
+   
+    return people[0];*/
+
+    //let s = people.sort((a,b) => a.yearOfBirth - a.yearOfDeath > b.yearOfBirth - b.yearOfDeath ? 1:-1);
+
+    let s = people.sort((a,b) => a.yearOfBirth - (a.yearOfDeath === undefined ? current:a.yearOfDeath) > b.yearOfBirth - (b.yearOfDeath === undefined ? current:b.yearOfDeath) ? 1:-1);
+
+
+    return s[0]
+
 
 };
 
